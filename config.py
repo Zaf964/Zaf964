6a81208650607589a73f8b9bf911ee85fdc56038
Zaf964 <138878897+Zaf964@users.noreply.github.com>
2023-08-02 16:26:52 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+
+TOKEN_USE_MAX_TIME_INTERVAL = 20
+
+MASTER_NAME="Aligator_4"
+#Telegram token
+TELE_TOKEN = '5617136781:AAHoztmSrQA9JZD6_El0UfWiXIm1MNDKuKw'
+STYLE_ENUM = 'precise'
+##########################"
+# bard settings
+BARD_TOKEN="ZAh7yws46T9Edi_rwYmVdehGfWKc8WuSOYp9JH77iecQXzffa8A9GNtFHAhpwwPRWOTRTg."
+
+
+BARD__Secure_1PSID ="ZAh7yws46T9Edi_rwYmVdehGfWKc8WuSOYp9JH77iecQXzffa8A9GNtFHAhpwwPRWOTRTg."
+BARD__Secure_1PSIDTS ="sidts-CjIBPu3jIdU-PB38YBTlcaiqx04lK0whlEXLqHRtIb5r2alVnp5vMQfMzEqBJrH4JhpqURAA"
+
+M_ID='1576462323'
+ST='app-h2h?key=pattystats&'
+
+
+
+
